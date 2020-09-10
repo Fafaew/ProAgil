d4b8002fdbc0c4e0d3bbfcb3d7349aa29a8eb807
Rafael Gomes <rafael.gomest@gmail.com>
2020-09-10 14:00:58 -0300

    Crud + Reactive forms

--- ProAgil-App/src/app/_services/evento.service.ts
+++ ProAgil-App/src/app/_services/evento.service.ts
@@ -8,19 +8,31 @@ import { Evento } from '../_models/Evento';
 })
 export class EventoService {
   baseURL = 'http://localhost:5000/api/evento';
-
-constructor(private http: HttpClient) { }
-
-getAllEvento(): Observable<Evento[]> {
-  return this.http.get<Evento[]>(this.baseURL);
+  
+  constructor(private http: HttpClient) { }
+  
+  getAllEvento(): Observable<Evento[]> {
+    return this.http.get<Evento[]>(this.baseURL);
+  }
+  
+  
+  getEventoByTema(tema: string): Observable<Evento[]> {
+    return this.http.get<Evento[]>(`${this.baseURL}/getBytema/${tema}`);
+  }
+  
+  getEventoById(id: number): Observable<Evento> {
+    return this.http.get<Evento>(`${this.baseURL}/${id}`);
+  }
+  
+  postEvento(evento: Evento){
+    return this.http.post(this.baseURL, evento);
+  }
+  
+  putEvento(evento: Evento){
+    return this.http.put(`${this.baseURL}/${evento.id}`, evento);
   }
 
-
-getEventoByTema(tema: string): Observable<Evento[]> {
-  return this.http.get<Evento[]>(`${this.baseURL}/getBytema/${tema}`);
-  }
-
-getEventoById(id: number): Observable<Evento> {
-  return this.http.get<Evento>(`${this.baseURL}/${id}`);
+  deleteEvento(id: number) {
+    return this.http.delete(`${this.baseURL}/${id}`);
   }
 }
--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
 
 
 
@@ -29,12 +31,15 @@ import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
+
   ],
   providers: [
     EventoService
